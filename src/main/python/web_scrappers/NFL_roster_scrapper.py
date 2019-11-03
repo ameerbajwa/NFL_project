b9dba83342bdc31cd49811802f47da017f29d884
ameerbajwa <ameerbajwa@gmail.com>
2019-11-03 11:59:50 -0800

    Rewriting the for loop to get all the active rosters of each NFL team for the 2019-2020 season. Changing to only 1 for loop instead of using 3. Still bugs that need to be fixed.

--- src/main/python/web_scrappers/NFL_roster_scrapper.py
+++ src/main/python/web_scrappers/NFL_roster_scrapper.py
@@ -14,33 +14,41 @@ driver.get("https://www.pro-football-reference.com/teams/")
 time.sleep(1)
 list_of_teams = driver.find_elements_by_xpath('/html/body/div[2]/div[3]/div[2]/div[2]/div/table/tbody//th')
 
-list_of_active_teams = []
+list_of_active_teams_roster = []
+print (len(list_of_teams))
+i = 0
+active_team_counter = 0
+while i < len(list_of_teams):
+    try:
+        print (i)
+        active_team = list_of_teams[i].find_element_by_tag_name('a').get_attribute('href')
+        print (active_team_counter)
 
-for i in range(0,len(list_of_teams)):
-    if (i < len(list_of_teams)):
-        try:
-            list_of_active_teams.append(
-                {
-                    'team_name': list_of_teams[i].find_element_by_tag_name('a').text,
-                    'team_url': list_of_teams[i].find_element_by_tag_name('a').get_attribute('href')
-                }
-            )
-        except NoSuchElementException:
-            pass
-    else:
-        break
+        list_of_active_teams_roster.append(
+            {
+                'team_name': list_of_teams[active_team_counter].find_element_by_tag_name('a').text,
+                'roster_url': ''
+            }
+        )
 
-print (list_of_active_teams)
+        driver.get(active_team)
+        time.sleep(2)
 
-for active_team in list_of_active_teams:
-    driver.get(active_team['team_url'])
-    time.sleep(1)
+        active_team_2019 = driver.find_element_by_xpath('//*[@id="team_index"]/tbody/tr[1]/td[2]/a').get_attribute('href')
+        driver.get(active_team_2019)
+        time.sleep(2)
 
-    team_roster_2019_2020_url = driver.find_element_by_xpath('//*[@id="team_index"]/tbody/tr[1]/td[2]/a').get_attribute('href')
-    print (team_roster_2019_2020_url)
-    active_team['team_url'] = team_roster_2019_2020_url
+        list_of_active_teams_roster[active_team_counter]['roster_url'] = driver.find_element_by_xpath('//*[@id="inner_nav"]/ul/li[5]/a').get_attribute('href')
+        print(list_of_active_teams_roster[active_team_counter])
+        i+=1
+        active_team_counter+=1
+        driver.get('https://www.pro-football-reference.com/teams/')
+        time.sleep(2)
+        list_of_teams = driver.find_elements_by_xpath('/html/body/div[2]/div[3]/div[2]/div[2]/div/table/tbody//th')
 
-print (list_of_active_teams)
+    except NoSuchElementException:
+        i+=1
+        pass
 
 sys.exit()
 
