55733682ee6582dba449dbe7d5e4ca0278cd0179
ameerbajwa <ameerbajwa@gmail.com>
2019-11-04 16:31:56 -0800

    Building/Testing automation of team's roster data being uploaded to the MySQL management database system.

--- src/main/python/SQL_uploads/insert.py
+++ src/main/python/SQL_uploads/insert.py
@@ -1,13 +1,57 @@
 import pymysql
 import pandas as pd
 
-connection_to_local_mysql_data_management_system = pd.read_csv('~/Desktop/connection_to_local_mysql_system.csv')
+def insert_roster_info_to_mysql(team_roster_info):
+    connection_to_local_mysql_data_management_system = pd.read_csv('~/Desktop/connection_to_local_mysql_system.csv')
 
-connection_to_database = pymysql.connect(
-                            user=connection_to_local_mysql_data_management_system.columns[0],
-                            password=connection_to_local_mysql_data_management_system.column[1],
-                            host=connection_to_local_mysql_data_management_system.column[2],
-                            port=connection_to_local_mysql_data_management_system.column[3],
-                            database='NFL_roster_info_2019_2020_season'
-                         )
+    connection_to_database = pymysql.connect(
+                                user=connection_to_local_mysql_data_management_system.columns[0],
+                                password=connection_to_local_mysql_data_management_system.column[1],
+                                host=connection_to_local_mysql_data_management_system.column[2],
+                                port=connection_to_local_mysql_data_management_system.column[3],
+                                database='NFL_database'
+                             )
+
+    for i in range(0, len(team_roster_info)):
+        insert_SQL_query = "INSERT INTO `NFL_roster_info_2019_2020_season` " \
+                           "(" \
+                           "    player_number," \
+                           "    player_name," \
+                           "    team," \
+                           "    age," \
+                           "    position," \
+                           "    games_played," \
+                           "    games_started," \
+                           "    weight," \
+                           "    height," \
+                           "    college," \
+                           "    birthdate," \
+                           "    experience," \
+                           "    drafted," \
+                           "    salary" \
+                           ") VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+
+        with connection_to_database.cursor() as cursor:
+            cursor.execute(insert_SQL_query,
+                            (
+                               team_roster_info.loc[i, 'No.'],
+                               team_roster_info.loc[i, 'Player '],
+                               team_roster_info.loc[i, 'Team'],
+                               team_roster_info.loc[i, 'Age'],
+                               team_roster_info.loc[i, 'Pos'],
+                               team_roster_info.loc[i, 'G'],
+                               team_roster_info.loc[i, 'GS'],
+                               team_roster_info.loc[i, 'Wt'],
+                               team_roster_info.loc[i, 'Ht'],
+                               team_roster_info.loc[i, 'College/Univ'],
+                               team_roster_info.loc[i, 'BirthDate'],
+                               team_roster_info.loc[i, 'Yrs'],
+                               team_roster_info.loc[i, 'Drafted (tm/rnd/yr)'],
+                               team_roster_info.loc[i, 'Salary']
+                            )
+                          )
+
+        connection_to_database.commit()
+        print(team_roster_info.loc[0, 'Team'] + 'roster insertion complete!')
+
 
---
+++ src/main/python/__init__.py
@@ -0,0 +1,2 @@
+from .SQL_uploads import insert
+from .web_scrappers import NFL_roster_scrapper
