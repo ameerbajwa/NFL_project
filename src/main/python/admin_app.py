3821f2666119b75d60a82d8dbd6233f86a051a5e
ameerbajwa <ameerbajwa@gmail.com>
2019-11-12 18:00:11 -0800

    Created a new python script that will only scrape urls that will be stored until use by team, player and play_by_play scrappers. Starting to build player scrapper script.

--- src/main/python/admin_app.py
+++ src/main/python/admin_app.py
@@ -1,4 +1,4 @@
-from src.main.python.web_scrappers import NFL_team_scrapper
+from src.main.python.web_scrappers import NFL_url_scrapper, NFL_team_scrapper, NFL_player_scrapper
 import sys
 
 exit_program = False
@@ -17,7 +17,10 @@ while (exit_program == False):
 
         if (scrapper_type == 'team'):
             type_of_info_from_teams = input('What type of team data do you want to be scrapped? ("roster" for roster data, "injury" for injury data, "team" for overall team statistics and personel information) ')
-            NFL_team_scrapper.grabbing_nfl_team_urls(type_of_info_from_teams, year)
+            NFL_url_scrapper.grabbing_nfl_team_urls(type_of_info_from_teams, year)
+        elif (scrapper_type == 'player_stats'):
+            week = input("Which week do you want player\'s data to be scrapped from?")
+            NFL_url_scrapper.grabbing_nfl_game_urls(year, week)
 
     if (action == 'create_inserter'):
         scrapper_type = input('What type of inserting scrapper do you want to create? ("team" for teams, "player_stats" for player stats, or "play_by_play" for play_by_play information) ')
--- src/main/python/web_scrappers/NFL_team_scrapper.py
+++ src/main/python/web_scrappers/NFL_team_scrapper.py
@@ -14,58 +14,6 @@ chromedriver = "/Applications/chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
 driver = webdriver.Chrome(chromedriver)
 
-def grabbing_nfl_team_urls(type_of_info_from_teams, year):
-    driver.get("https://www.pro-football-reference.com/teams/")
-    time.sleep(1)
-    list_of_teams = driver.find_elements_by_xpath('/html/body/div[2]/div[3]/div[2]/div[2]/div/table/tbody//th')
-
-    list_of_active_teams = []
-    i = 0
-    team_counter = 0
-
-    while i < len(list_of_teams):
-        try:
-            active_team = list_of_teams[i].find_element_by_tag_name('a').get_attribute('href')
-
-            list_of_active_teams.append(
-                {
-                    'team_name': list_of_teams[i].find_element_by_tag_name('a').text,
-                    'url': ''
-                }
-            )
-
-            driver.get(active_team)
-            time.sleep(2)
-
-            active_teams = driver.find_elements_by_xpath('//*[@id="team_index"]/tbody//tr')
-            for team in active_teams:
-                if (team.find_element_by_tag_name('th').text == str(year)):
-                    active_team = team.find_element_by_xpath('td[2]/a').get_attribute('href')
-
-            driver.get(active_team)
-            time.sleep(2)
-            
-            if (type_of_info_from_teams == 'roster'):
-                list_of_active_teams[team_counter]['url'] = driver.find_element_by_xpath('//*[@id="inner_nav"]/ul/li[5]/a').get_attribute('href')
-            elif (type_of_info_from_teams == 'injury'):
-                list_of_active_teams[team_counter]['url'] = driver.find_element_by_xpath('//*[@id="inner_nav"]/ul/li[8]/a').get_attribute('href')
-            elif (type_of_info_from_teams == 'team'):
-                list_of_active_teams[team_counter]['url'] = active_team
-
-            i += 1
-            team_counter += 1
-
-            driver.get('https://www.pro-football-reference.com/teams/')
-            time.sleep(2)
-            list_of_teams = driver.find_elements_by_xpath('/html/body/div[2]/div[3]/div[2]/div[2]/div/table/tbody//th')
-
-        except NoSuchElementException:
-            i += 1
-            continue
-
-    with open('dictionaries_of_nfl_urls/list_of_active_teams_' + type_of_info_from_teams + '_data_for_season_' + year, 'wb') as handle:
-        pickle.dump(list_of_active_teams, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
 def selecting_info(type_of_info_from_teams, year):
     with open('dictionaries_of_nfl_urls/list_of_active_teams_' + type_of_info_from_teams + '_data_for_season_' + year, 'rb') as handle:
         list_of_active_teams = pickle.load(handle)
