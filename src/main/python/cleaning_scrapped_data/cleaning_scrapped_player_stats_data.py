ca034c15e152a29443beb44c6a16163f684f4e35
ameerbajwa <ameerbajwa@gmail.com>
2019-11-14 08:32:19 -0800

    Creating the scrappers for grabbing offensive and defensive player stats for each game. Created a new function table scrapper that will scrape any table for any game's box score.

---
+++ src/main/python/cleaning_scrapped_data/cleaning_scrapped_player_stats_data.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+def cleaning_offensive_player_stats(basic_off_player_stats_df, adv_off_player_stats_df):
+
--- src/main/python/web_scrappers/NFL_player_scrapper.py
+++ src/main/python/web_scrappers/NFL_player_scrapper.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 from src.main.python.SQL_uploads import insert
-from src.main.python.cleaning_scrapped_data import cleaning_scrapped_team_data
+from src.main.python.cleaning_scrapped_data import cleaning_scrapped_player_stats_data
 
 import pandas as pd
 import time
@@ -22,37 +22,54 @@ def selecting_player_info(year, week):
     grab_defensive_player_data(dict_of_game_summaries)
     grab_special_teams_player_data(dict_of_game_summaries)
 
+def table_scrapper(id_of_table):
+
+    # unique ids_for_table scrapping of player stats by game: player_offense, passing_advanced, rushing_advanced, receiving_advanced, player_defense, defense_advanced
+
+    raw_column_names = driver.find_elements_by_xpath('//*[@id="' + id_of_table +'"]/thead/tr[2]//th') # id_of_table
+    column_names = []
+
+    for col in raw_column_names:
+        column_names.append(col.text)
+
+    player_stats_df = pd.DataFrame(columns=column_names)
+
+    player_stats = driver.find_elements_by_xpath('//*[@id="'+ id_of_table + '"]/tbody//tr') # id_of_table
+
+    for player in player_stats:
+        player_stats = {}
+        if (len(player.find_elements_by_tag_name('th')) > 1):
+            continue
+        else:
+            player_stats[column_names[0]] = player.find_element_by_xpath('/th/a').text
+
+            list_of_stats = []
+            for stat in player.find_elements_by_tag_name('td'):
+                list_of_stats.append(stat.text)
+
+            for i in range(1, len(column_names)):
+                player_stats[column_names[i]] = list_of_stats[i - 1]
+
+            player_stats_df = player_stats_df.append(player_stats, ignore_index=True)
+
+    return (player_stats_df)
+
 def grab_offensive_player_data(dict_of_game_summaries):
 
     for game_summary in dict_of_game_summaries['list_of_game_summary_urls']:
         driver.get(game_summary)
         time.sleep(1)
 
-        raw_basic_off_column_names = driver.find_elements_by_xpath('//*[@id="player_offense"]/thead/tr[2]//th')
-        basic_off_column_names = []
+        basic_off_player_stats_df = table_scrapper('player_offense')
+        adv_passing_player_stats_df = table_scrapper('passing_advanced')
+        adv_rushing_player_stats_df = table_scrapper('rushing_advanced')
+        adv_receiving_player_stats_df = table_scrapper('receiving_advanced')
 
-        for basic_off_col in raw_basic_off_column_names:
-            basic_off_column_names.append(basic_off_col.text)
+        clean_passing_stats_df = cleaning_scrapped_player_stats_data.cleaning_offensive_player_stats(basic_off_player_stats_df, adv_passing_player_stats_df)
+        clean_rushing_stats_df = cleaning_scrapped_player_stats_data.cleaning_offensive_player_stats(basic_off_player_stats_df, adv_rushing_player_stats_df)
+        clean_receiving_stats_df = cleaning_scrapped_player_stats_data.cleaning_offensive_player_stats(basic_off_player_stats_df, adv_receiving_player_stats_df)
 
-        basic_off_player_stats_df = pd.DataFrame(columns=basic_off_column_names)
 
-        basic_off_player_stats = driver.find_elements_by_xpath('//*[@id="player_offense"]/tbody//tr')
-
-        for player in basic_off_player_stats:
-            player_stats = {}
-            if (len(player.find_elements_by_tag_name('th')) > 1):
-                continue
-            else:
-                player_stats[basic_off_column_names[0]] = player.find_element_by_xpath('/th/a').text
-
-                list_of_stats = []
-                for stat in player.find_elements_by_tag_name('td'):
-                    list_of_stats.append(stat.text)
-
-                for i in range(1,len(basic_off_column_names)):
-                    player_stats[basic_off_column_names[i]] = list_of_stats[i-1]
-
-                basic_off_player_stats_df = basic_off_player_stats_df.append(player_stats, ignore_index=True)
 
 
 def grab_defensive_player_data(dict_of_game_summaries):
@@ -60,32 +77,6 @@ def grab_defensive_player_data(dict_of_game_summaries):
     for game_summary in dict_of_game_summaries['list_of_game_summary_urls']:
         driver.get(game_summary)
         time.sleep(1)
-
-    raw_basic_def_column_names = driver.find_elements_by_xpath('//*[@id="player_defense"]/thead/tr[2]//th')
-    basic_def_column_names = []
-
-    for basic_def_col in raw_basic_def_column_names:
-        basic_def_column_names.append(basic_def_col.text)
-
-    basic_def_player_stats_df = pd.DataFrame(columns=basic_def_column_names)
-
-    basic_def_player_stats = driver.find_elements_by_xpath('//*[@id="player_defense"]/tbody//tr')
-
-    for player in basic_def_player_stats:
-        player_stats = {}
-        if (len(player.find_elements_by_tag_name('th')) > 1):
-            continue
-        else:
-            player_stats[basic_def_column_names[0]] = player.find_element_by_xpath('/th/a').text
-
-            list_of_stats = []
-            for stat in player.find_elements_by_tag_name('td'):
-                list_of_stats.append(stat.text)
-
-            for i in range(1, len(basic_def_column_names)):
-                player_stats[basic_def_column_names[i]] = list_of_stats[i - 1]
-
-            basic_def_player_stats_df = basic_def_player_stats_df.append(player_stats, ignore_index=True)
 
 def grab_special_teams_player_data(dict_of_game_summaries):
 
