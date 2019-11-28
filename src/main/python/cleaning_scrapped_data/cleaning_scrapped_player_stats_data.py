e01b09ff23e01a2467339d7f28807769f76c64ee
ameerbajwa <ameerbajwa@gmail.com>
2019-11-27 16:44:26 -0800

    Created cleaning function for player stats. In debugging and testing phase for cleaning player stats. - Finished the offensive player stats cleaning functions. Now working on defensive and special teams stats cleaning functions. Shouldn't be much but I forgot to add basic game data for each player stats dataframe so I would know who had what game at which game and against whom. So I have to add that bit of scrapping and cleaning for the dates. Will just put this new basic game info in a new dataframe.

--- src/main/python/cleaning_scrapped_data/cleaning_scrapped_player_stats_data.py
+++ src/main/python/cleaning_scrapped_data/cleaning_scrapped_player_stats_data.py
@@ -1,12 +1,13 @@
-import pandas as pd
 
 def converting_to_int(x):
+
     if (x == ''):
         return 0
     else:
         return int(x)
 
 def converting_to_float(x):
+
     if (x == ''):
         return 0.0
     else:
@@ -16,62 +17,91 @@ def convert_values_to_appropriate_types(basic_player_stats_df, adv_player_stats_
 
     for df in [basic_player_stats_df, adv_player_stats_df]:
         for col in df.columns[2:]:
-            print (col)
-            if ('%' in df.loc[0, col]):
-                df[col] = list(map(lambda x: x.replace('%', ''), df[col]))
-
-            if ('.' in df.loc[0, col]):
-                df[col] = list(map(lambda x: 0.0 if '' else float(x), df[col]))
+            if ('%' in df[col].values):
+                df[col] = list(map(lambda x: x.replace('%', '') if '%' in x else x, df[col]))
             else:
-                df[col] = list(map(lambda x: 0 if '' else int(x), df[col]))
-            print ('conversion complete')
+                continue
+
+            if ('.' in df[col].values):
+                df[col] = list(map(converting_to_float, df[col]))
+            else:
+                df[col] = list(map(converting_to_int, df[col]))
 
     return (basic_player_stats_df, adv_player_stats_df)
 
-def joining_basic_stats_and_adv_stats(basic_player_stats_df, adv_player_stats_df, list_of_new_column_names, identifier):
+def defining_new_columns(list_of_new_column_names, cleaner_adv_off_player_stats_df):
+
+    for col in list_of_new_column_names:
+        cleaner_adv_off_player_stats_df[col] = 0
+
+    return (cleaner_adv_off_player_stats_df)
+
+def joining_basic_stats_and_adv_stats(basic_player_stats_df, adv_player_stats_df, list_of_new_column_names):
+
     counter = 0
     for row_index in range(0, len(basic_player_stats_df)):
-        if (basic_player_stats_df.loc[row_index, identifier] == 0):
-            continue
+        if (counter < len(adv_player_stats_df)):
+            if (basic_player_stats_df.loc[row_index, 'Player'] == adv_player_stats_df.loc[counter, 'Player']):
+                for col in list_of_new_column_names:
+                    adv_player_stats_df.loc[counter, col] = basic_player_stats_df.loc[row_index, col]
+                counter += 1
         else:
-            for col in list_of_new_column_names:
-                adv_player_stats_df.loc[counter, col] = basic_player_stats_df.loc[row_index, col]
-            counter += 1
+            continue
 
     return (adv_player_stats_df)
 
 def cleaning_offensive_player_stats(basic_off_player_stats_df, adv_off_player_stats_df, type_of_offense):
 
+    cleaner_basic_off_player_stats_df, cleaner_adv_off_player_stats_df = convert_values_to_appropriate_types(basic_off_player_stats_df, adv_off_player_stats_df)
+
     if (type_of_offense == 'passing'):
-
-        # ORDER PANDAS DATAFRAME BY TEAM NAME AND THEN PASSING YARDS AND THEN FILL IN MERGE THE TWO TABLES OF STATS USING
-        #  THE COUNTER
-
-        cleaner_basic_off_player_stats_df, cleaner_adv_off_player_stats_df = convert_values_to_appropriate_types(basic_off_player_stats_df, adv_off_player_stats_df)
 
         cleaner_basic_off_player_stats_df.sort_values(by=['Tm', 'Passing_Yds', 'Player'], ascending=[False, True, False], inplace=True)
         cleaner_adv_off_player_stats_df.sort_values(by=['Tm', 'Yds', 'Player'], ascending=[False, True, False], inplace=True)
 
-        list_of_new_column_names = ['Passing_TD', 'Passing_Int', 'Passing_sacked_Yds', 'Passing_Lng', 'Passing_Rate']
+        list_of_new_column_names = ['Passing_TD', 'Passing_Int', 'Passing_sacked_Yds', 'Passing_Lng', 'Passing_Rate', 'Fumbles_Fmb', 'Fumbles_FL']
 
-        for col in list_of_new_column_names:
-            adv_off_player_stats_df[col] = 0
+        new_adv_off_player_stats_df = defining_new_columns(list_of_new_column_names, cleaner_adv_off_player_stats_df)
 
-        passing_stats_df = joining_basic_stats_and_adv_stats(cleaner_basic_off_player_stats_df, cleaner_adv_off_player_stats_df, list_of_new_column_names, 'Cmp')
+        passing_stats_df = joining_basic_stats_and_adv_stats(cleaner_basic_off_player_stats_df, new_adv_off_player_stats_df, list_of_new_column_names)
 
+        print (passing_stats_df)
         return (passing_stats_df)
 
     elif (type_of_offense == 'rushing'):
 
-        # ORDER PANDAS DATAFRAME BY TEAM NAME AND THEN RUSHING ATTEMPTS AND THEN FILL IN MERGE THE TWO TABLES OF STATS USING
-        #  THE COUNTER
-        basic_off_player_stats_df.sort_values(['Tm', 'Rushing_Att', 'Player'], axis=1, inplace=True)
-        adv_off_player_stats_df.sort_values(['Tm', 'Att', 'Player'], axis=1, inplace=True)
+        cleaner_basic_off_player_stats_df.sort_values(['Tm', 'Rushing_Att', 'Player'], ascending=[False, True, False], inplace=True)
+        cleaner_adv_off_player_stats_df.sort_values(['Tm', 'Att', 'Player'], ascending=[False, True, False], inplace=True)
+
+        list_of_new_column_names = ['Rushing_TD', 'Rushing_Lng', 'Fumbles_Fmb', 'Fumbles_FL']
+
+        new_adv_off_player_stats_df = defining_new_columns(list_of_new_column_names, cleaner_adv_off_player_stats_df)
+
+        rushing_stats_df = joining_basic_stats_and_adv_stats(cleaner_basic_off_player_stats_df, new_adv_off_player_stats_df, list_of_new_column_names)
+
+        print (rushing_stats_df)
+        return (rushing_stats_df)
 
     elif (type_of_offense == 'receiving'):
 
-        basic_off_player_stats_df.sort_values(['Tm', 'Receiving_Tgt', 'Player'], axis=1, inplace=True)
-        adv_off_player_stats_df.sort_values(['Tm', 'Tgt', 'Player'], axis=1, inplace=True)
+        cleaner_basic_off_player_stats_df.sort_values(['Tm', 'Receiving_Tgt', 'Player'], ascending=[False, True, False], inplace=True)
+        cleaner_adv_off_player_stats_df.sort_values(['Tm', 'Tgt', 'Player'], ascending=[False, True, False], inplace=True)
 
+        list_of_new_column_names = ['Receiving_TD', 'Receiving_Lng', 'Fumbles_Fmb', 'Fumbles_FL']
 
+        new_adv_off_player_stats_df = defining_new_columns(list_of_new_column_names, cleaner_adv_off_player_stats_df)
+
+        receiving_stats_df = joining_basic_stats_and_adv_stats(cleaner_basic_off_player_stats_df, new_adv_off_player_stats_df, list_of_new_column_names)
+
+        print (receiving_stats_df)
+        return (receiving_stats_df)
+
+def cleaning_defensive_player_stats(basic_def_player_stats_df, adv_def_player_stats_df):
+
+    cleaner_basic_def_player_stats_df, cleaner_adv_def_player_stats_df = convert_values_to_appropriate_types(basic_def_player_stats_df, adv_def_player_stats_df)
+
+    cleaner_basic_def_player_stats_df.sort_values(['Tm', 'Tackles_Comb', 'Player'], ascending=[False, True, False], inplace=True)
+    cleaner_adv_def_player_stats_df.sort_values(['Tm', 'Comb', 'Player'], ascending=[False, True, False], inplace=True)
+
+    list_of_new_columns_names =
 
